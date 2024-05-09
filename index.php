44cfb216bf6fad08bad617f39cdb0774af706616
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-09 14:42:37 +0200

    Add → added bootstrap, vue and linked my stylesheet to project

--- index.php
+++ index.php
@@ -48,3 +48,26 @@ var_dump($film_lo_hobbit);
 $film_onore_dei_ladri = new Production('L\'onore dei ladri', 'Marziano', 8);
 
 var_dump($film_onore_dei_ladri);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    
+<div id="app">
+    
+    {{ message }}
+
+
+</div>
+
+ <script src="./js/app.js"></script>
+</body>
+</html>
