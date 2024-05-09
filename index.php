17b3c05ace68a8f1c496c40eacf9895f1aff23a0
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-09 15:06:21 +0200

    Add → added new Production istances and page printing of all Productions

--- index.php
+++ index.php
@@ -36,18 +36,30 @@ class Production
 
 // Qui andrò a creare i due oggetti, istanze della classe production.
 
-$film_il_signore_degli_anelli = new Production('Il Signore degli Anelli', 'Italiano', 10);
-
-var_dump($film_il_signore_degli_anelli);
+$film_1 = new Production('Il Signore degli Anelli', 'Italiano', 10);
 
 
-$film_lo_hobbit = new Production('Lo Hobbit', 'Inglese', 9);
+$film_2 = new Production('Lo Hobbit', 'Inglese', 9);
 
-var_dump($film_lo_hobbit);
 
-$film_onore_dei_ladri = new Production('L\'onore dei ladri', 'Marziano', 8);
+$film_3 = new Production('L\'onore dei ladri', 'Marziano', 8);
 
-var_dump($film_onore_dei_ladri);
+
+
+$film_4 = new Production('Il film che non esiste', 'Nessuna', 1);
+
+
+$film_5 = new Production('Un film a caso', 'Corta', 4);
+
+
+$film_6 = new Production('Il programmatore', 'JavaScript', 7);
+
+
+$film_7 = new Production('Il sesto Film', 'Elfico', 6);
+
+
+$film_8 = new Production('Un film brutto', 'Incomprensibile', 1);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -72,7 +84,93 @@ var_dump($film_onore_dei_ladri);
         </nav>
     </header>
     
+    <main>
+        <section class="films-section py-5">
+            <div class="container">
+                <div class="row g-5">
+                    <div class="col-3">
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_1->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_1->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_1->vote ?></p>
+                            </div>
+                        </div>                       
+                    </div>
+                    
+                    <div class="col-3">
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_2->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_2->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_2->vote ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-3">                        
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_3->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_3->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_3->vote ?></p>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="col-3">
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_4->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_4->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_4->vote ?></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_5->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_5->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_5->vote ?></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_6->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_6->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_6->vote ?></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_7->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_7->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_7->vote ?></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="card px-2 py-5 text-bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title text-center text-crimson"><?php echo $film_8->title ?></h5>
+                                <p class="card-text py-4">Lingua: <?php echo $film_8->language ?></p>
+                                <p class="card-text">Voto: <?php echo $film_8->vote ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
 </div>
 
