7dc4a6243ea364ccdb30656c4d7ad9be618de500
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-10 15:09:19 +0200

    Add → added values to Movie and TVSerie classes

--- models/Movie.php
+++ models/Movie.php
@@ -1,1 +1,8 @@
 <?php
+require_once __DIR__ . '/Production.php';
+
+class Movie
+{
+    public $profitti;
+    public $durata;
+}
--- models/TVSerie.php
+++ models/TVSerie.php
@@ -1,1 +1,7 @@
 <?php
+require_once __DIR__ . '/Production.php';
+
+class TVSerie
+{
+    public $numero_stagioni;
+}
