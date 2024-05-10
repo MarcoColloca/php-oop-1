2bbd59aa12e20bf746ca8b2a6e516b961ac9b547
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-10 15:45:48 +0200

    Add → populated Movie and TVSerie Classes and constructed them with parent constructor (Production)

--- models/Movie.php
+++ models/Movie.php
@@ -1,8 +1,20 @@
 <?php
 require_once __DIR__ . '/Production.php';
 
-class Movie
-{
+class Movie extends Production
+{   
+    
     public $profitti;
     public $durata;
+
+
+    function __construct(string $_title, string $_language, int $_vote, Genre $_genre, int $_profitti, int $_durata)
+    {
+ 
+        parent::__construct($_title, $_language, $_vote, $_genre);
+
+        $this->profitti = $_profitti;
+        $this->durata = $_durata;
+
+    }
 }
--- models/TVSerie.php
+++ models/TVSerie.php
@@ -1,7 +1,15 @@
 <?php
 require_once __DIR__ . '/Production.php';
 
-class TVSerie
+class TVSerie extends Production
 {
     public $numero_stagioni;
+
+    function __construct(string $_title, string $_language, int $_vote, Genre $_genre, int $numero_stagioni)
+    {
+ 
+        parent::__construct($_title, $_language, $_vote, $_genre);
+
+        $this->numero_stagioni = $numero_stagioni;
+    }
 }
